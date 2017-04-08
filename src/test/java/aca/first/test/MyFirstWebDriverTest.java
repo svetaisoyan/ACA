6ea9fbfb596d399504fb504167e7b65af2ad1136
svetaisoyan <sv.isoyan@gmail.com>
2017-04-08 13:34:28 +0500

    Test.

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,9 +1,10 @@
 package aca.first.test;
 
 import org.testng.annotations.Test;
+import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,30 +15,17 @@ public class MyFirstWebDriverTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Администратор\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		 WebDriver driver = new ChromeDriver();
 		 driver.get("http://www.youtube.com");
-		 //Test Case
-		 //Description:Verifying URL
-		 //Steps
 		 //1.Open browser
 		 //2.Enter youtube URL "https://www.youtube.com/"
-		 //Expected results
-		 //Expected Browser should be opened and Browser should navigate to youtube home page.
-		 boolean present;
-		 try {
-		    driver.findElement(By.id("search-btn"));
-		    present = true;
-		 } catch (NoSuchElementException exception) {
-			
-		    present = false;
-		 }
-		 boolean present2;
-		 try {
-		    driver.findElement(By.id("upload-btn"));
-		    present2 = true;
-		 } catch (NoSuchElementException exception) {
-			
-		    present2 = false;
-         driver.close();
+		 //3.Search something
+		 WebElement search = driver.findElement(By.id("masthead-search-term"));
+		 search.sendKeys("Git tutorial");
+		 //4.Click search button
+		 WebElement click = driver.findElement(By.id("search-btn"));
+		 click.click();
+		 //5.Open the page by searching Git tutorial
+		 
 		 driver.quit(); 
 		 }
-	}
+
 }
