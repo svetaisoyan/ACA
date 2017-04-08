b40e1537a82d5ae75c38ae2559cb65f340cd967a
svetaisoyan <sv.isoyan@gmail.com>
2017-04-08 15:08:50 +0500

    Youtube Test.

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -15,17 +15,25 @@ public class MyFirstWebDriverTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Администратор\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		 WebDriver driver = new ChromeDriver();
 		 driver.get("http://www.youtube.com");
-		 //1.Open browser
-		 //2.Enter youtube URL "https://www.youtube.com/"
-		 //3.Search something
+		 //1.Search something
 		 WebElement search = driver.findElement(By.id("masthead-search-term"));
 		 search.sendKeys("Git tutorial");
-		 //4.Click search button
+		 //2.Click search button
 		 WebElement click = driver.findElement(By.id("search-btn"));
 		 click.click();
-		 //5.Open the page by searching Git tutorial
+		 //3.Open the page by searching Git tutorial
+		 Assert.assertTrue("Git tutorial for beginners" !=null);
+		 //7.Open the first video of the loaded page.
+		 WebElement firstvideo = driver.findElement(By.xpath("//ol[@class='item-section']//a[2]"));
+		 firstvideo.click();
+		 Assert.assertTrue(driver.findElement(By.id("watch-headline-title" !=null)));
+		
 		 
-		 driver.quit(); 
-		 }
+		 
+		 
+		 
+	
+	}
+	
 
 }
